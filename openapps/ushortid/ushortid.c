9903032df2fdf11890d762eb95f4fc15549a818a
neol-d2022 <neol_d2022@outlook.com>
2017-03-14 19:09:02 +0800

    fixed time out value

--- openapps/ushortid/ushortid.c
+++ openapps/ushortid/ushortid.c
@@ -20,7 +20,7 @@
 
 /// inter-packet period (in ms)
 #define USHORTIDPERIOD       1000
-#define USHORTIDTIMEOUTMUL   20
+#define USHORTIDTIMEOUT      20000
 
 #define USHORTIDPAYLOADLEN  10
 
@@ -39,11 +39,11 @@ void ushortid_init() {
    ushortid_vars.timerId_ushortid          = opentimers_start(USHORTIDPERIOD,
                                                 TIMER_PERIODIC,TIME_MS,
                                                 ushortid_timer_cb);
-   ushortid_vars.timerId_ushortid_timeout  = opentimers_start(USHORTIDTIMEOUTMUL,
+   /*ushortid_vars.timerId_ushortid_timeout  = opentimers_start(USHORTIDTIMEOUT,
                                                 TIMER_ONESHOT,TIME_MS,
                                                 ushortid_timeout_timer_cb);
-   opentimers_stop(ushortid_vars.timerId_ushortid_timeout);
-   ushortid_vars.backoff = 20;
+   opentimers_stop(ushortid_vars.timerId_ushortid_timeout);*/
+   ushortid_vars.backoff = 60;
 }
 
 //=========================== private =========================================
@@ -61,6 +61,7 @@ void ushortid_receive(OpenQueueEntry_t* request) {
             sid += ((uint8_t)request->payload[9]) << 0;
             if(ushortid_vars.askingSelf==TRUE) {
                 ushortid_vars.mysid = sid;
+                //printf("%hhu got reply %hu asking for short id of %02hhx:%02hhx:%02hhx:%02hhx:%02hhx:%02hhx:%02hhx:%02hhx\n", (idmanager_getMyID(ADDR_64B)->addr_64b)[7], sid, ushortid_vars.desireAddr[0], ushortid_vars.desireAddr[1], ushortid_vars.desireAddr[2], ushortid_vars.desireAddr[3], ushortid_vars.desireAddr[4], ushortid_vars.desireAddr[5], ushortid_vars.desireAddr[6], ushortid_vars.desireAddr[7]);
             }
             else {
                 addr.type = ADDR_64B;
@@ -68,14 +69,16 @@ void ushortid_receive(OpenQueueEntry_t* request) {
                 nidx = neighbors_addressToIndex(&addr);
                 if (nidx < MAXNUMNEIGHBORS) {
                     neighbors_set_ushortid(nidx, sid);
+                    //printf("%hhu got reply %hu asking for short id of %02hhx:%02hhx:%02hhx:%02hhx:%02hhx:%02hhx:%02hhx:%02hhx\n", (idmanager_getMyID(ADDR_64B)->addr_64b)[7], sid, ushortid_vars.desireAddr[0], ushortid_vars.desireAddr[1], ushortid_vars.desireAddr[2], ushortid_vars.desireAddr[3], ushortid_vars.desireAddr[4], ushortid_vars.desireAddr[5], ushortid_vars.desireAddr[6], ushortid_vars.desireAddr[7]);
                 }
                 else {
                     // OOPS, addr is not neighbor nor self addr
+                    //printf("%hhu got reply %hu but for %02hhx:%02hhx:%02hhx:%02hhx:%02hhx:%02hhx:%02hhx:%02hhx, asking for short id of %02hhx:%02hhx:%02hhx:%02hhx:%02hhx:%02hhx:%02hhx:%02hhx\n", (idmanager_getMyID(ADDR_64B)->addr_64b)[7], sid, request->payload[0], request->payload[1], request->payload[2], request->payload[3], request->payload[4], request->payload[5], request->payload[6], request->payload[7], ushortid_vars.desireAddr[0], ushortid_vars.desireAddr[1], ushortid_vars.desireAddr[2], ushortid_vars.desireAddr[3], ushortid_vars.desireAddr[4], ushortid_vars.desireAddr[5], ushortid_vars.desireAddr[6], ushortid_vars.desireAddr[7]);
                 }
             }
             memset(ushortid_vars.desireAddr,0,sizeof(ushortid_vars.desireAddr));
-            ushortid_vars.waitingRes = FALSE;
             ushortid_vars.askingSelf = FALSE;
+            ushortid_vars.waitingRes = FALSE;
         }
     }
 	openqueue_freePacketBuffer(request);
@@ -91,8 +94,9 @@ void ushortid_timeout_timer_cb(opentimer_id_t id){
    if(ushortid_vars.waitingRes==TRUE) {
       ushortid_vars.waitingRes = FALSE; //Query timed out
       ushortid_vars.askingSelf = FALSE;
+      //printf("%hhu timed out asking for short id of %02hhx:%02hhx:%02hhx:%02hhx:%02hhx:%02hhx:%02hhx:%02hhx\n", (idmanager_getMyID(ADDR_64B)->addr_64b)[7], ushortid_vars.desireAddr[0], ushortid_vars.desireAddr[1], ushortid_vars.desireAddr[2], ushortid_vars.desireAddr[3], ushortid_vars.desireAddr[4], ushortid_vars.desireAddr[5], ushortid_vars.desireAddr[6], ushortid_vars.desireAddr[7]);
       memset(ushortid_vars.desireAddr,0,sizeof(ushortid_vars.desireAddr));
-      ushortid_vars.backoff = 10;
+      ushortid_vars.backoff = 20;
    }
 }
 
@@ -126,13 +130,16 @@ void ushortid_task_cb() {
       // don't continue if I'm still sending a previous data packet
       return;
    }
+   ushortid_vars.busySendingData = TRUE;
 
    if(ushortid_vars.waitingRes==TRUE) {
+      ushortid_vars.busySendingData = FALSE;
       return;
    }
 
    if(ushortid_vars.backoff != 0) {
       ushortid_vars.backoff = ushortid_vars.backoff - 1;
+      ushortid_vars.busySendingData = FALSE;
       return;
    }
 
@@ -147,7 +154,10 @@ void ushortid_task_cb() {
          neighbors_getNeighborEui64(&nadd64, ADDR_64B, nidx);
          add64 = &nadd64;
       }
-      else return ;
+      else {
+         ushortid_vars.busySendingData = FALSE;
+         return ;
+      }
    }
    
 
@@ -159,6 +169,7 @@ void ushortid_task_cb() {
          (errorparameter_t)0,
          (errorparameter_t)0
       );
+      ushortid_vars.busySendingData = FALSE;
       return;
    }
    // take ownership over that packet
@@ -178,25 +189,24 @@ void ushortid_task_cb() {
    memcpy(&pkt->l3_destinationAdd.addr_128b[0],&ipAddr_RootShortID,16);
 
    // send
-   ushortid_vars.busySendingData = TRUE;
    ushortid_vars.askingSelf = askingSelf;
    memcpy(ushortid_vars.desireAddr,add64->addr_64b,sizeof(ushortid_vars.desireAddr));
+   //printf("%hhu asking for short id of %02hhx:%02hhx:%02hhx:%02hhx:%02hhx:%02hhx:%02hhx:%02hhx\n", (idmanager_getMyID(ADDR_64B)->addr_64b)[7], ushortid_vars.desireAddr[0], ushortid_vars.desireAddr[1], ushortid_vars.desireAddr[2], ushortid_vars.desireAddr[3], ushortid_vars.desireAddr[4], ushortid_vars.desireAddr[5], ushortid_vars.desireAddr[6], ushortid_vars.desireAddr[7]);
    if ((openudp_send(pkt))==E_FAIL) {
       openqueue_freePacketBuffer(pkt);
-      ushortid_vars.busySendingData = FALSE;
       ushortid_vars.askingSelf = FALSE;
       memset(ushortid_vars.desireAddr,0,sizeof(ushortid_vars.desireAddr));
-      ushortid_vars.backoff = 10;
+      ushortid_vars.backoff = 15;
+      ushortid_vars.busySendingData = FALSE;
    }
 
    return;
 }
 
 void ushortid_sendDone(OpenQueueEntry_t* msg, owerror_t error) {
-   ushortid_vars.busySendingData = FALSE;
    ushortid_vars.waitingRes = TRUE;
-   opentimers_setPeriod(ushortid_vars.timerId_ushortid_timeout,TIMER_ONESHOT,USHORTIDTIMEOUTMUL*(uint32_t)icmpv6rpl_getMyDAGrank());
-   opentimers_restart(ushortid_vars.timerId_ushortid_timeout);
+   ushortid_vars.busySendingData = FALSE;
+   ushortid_vars.timerId_ushortid_timeout  = opentimers_start(USHORTIDTIMEOUT, TIMER_ONESHOT,TIME_MS, ushortid_timeout_timer_cb);
    openqueue_freePacketBuffer(msg);
 }
 
