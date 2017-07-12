56771b99d2db2bed0dd650af9456e07659b1fd7a
neol-d2022 <neol_d2022@outlook.com>
2017-07-12 23:19:10 +0800

    Timeout adjust

--- openstack/02b-MAChigh/sixtop.h
+++ openstack/02b-MAChigh/sixtop.h
@@ -91,7 +91,8 @@ typedef enum {
 }metadata_t;
 //=========================== typedef =========================================
 
-#define SIX2SIX_TIMEOUT_MS 4000
+#define SIX2SIX_TIMEOUT_SLOTFRAME 16
+#define SIX2SIX_TIMEOUT_MS (SIX2SIX_TIMEOUT_SLOTFRAME * SLOTFRAME_LENGTH * 15)
 typedef uint8_t                 (*sixtop_sf_getsfid_cbt)(void);
 typedef uint16_t                (*sixtop_sf_getmetadata_cbt)(void);
 typedef metadata_t              (*sixtop_sf_translatemetadata_cbt)(void);
